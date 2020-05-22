27f3827c4be86454f7d24caf9682cfb3bf9c667c
Cory Marshman <marshman.cory@gmail.com>
2020-05-22 14:03:04 -0400

    Our culture tablet version, complete

--- src/components/OurCulture/index.js
+++ src/components/OurCulture/index.js
@@ -8,26 +8,28 @@ const OurCulture = () => {
             <div className="container">
                 <p className="has-text-centered" id="culture">Our Culture</p>
                 <p className="has-text-centered is-centered" id="description">At Saleswhale, we encourage everyone to grow and innovate, while having fun in the process. </p>
-                <div className="tile columns is-centered is-10" id="change">
-                    <div className="column is-one-third">
+                <div className="tile is-10" id="change">
+                    <div className="columns is-multiline">
+                    <div className="column is-one-third" id="column">
                         <img src={Anime} alt='anime character' id="animePic"/>
                         <p id="subtitle">Building for the future</p>
                         <p id="describe">We always challenge the status quo and work hard innovating on a product that not only works, but changes the game for our users. </p>
                     </div>
-                    <div className="column is-one-third">
+                    <div className="column is-one-third" id="column">
                         <img src={Anime} alt='anime character' id="animePic"/>
                         <p id="subtitle">Promoting trust & transparency</p>
                         <p id="describe">We remain completely open, honest and transparent with everyone. This has allowed us to build strong relationships with each other.</p>
                     </div>
-                    <div className="column is-one-third">
+                    <div className="column is-one-third" id="column">
                         <img src={Anime} alt='anime character' id="animePic"/>
                         <p id="subtitle">Achieving our very best</p>
                         <p id="describe">We believe the success of the company lies within our employees and we constantly challenge each other to learn and level up.</p>
                     </div>
+                    </div>
                 </div>
                 <p className="has-text-centered" id="talk">If you are excited by our vision of the future, and want to solve meaningful problems, talk to us!</p>
                 <div className="columns" id="centerBtn">
-                    <a className="button is-primary" href="*" id="btn">
+                    <a className="column is-4 is-offset-4 button is-primary" href="*" id="btn">
                         Join the team
                     </a>
                 </div>
